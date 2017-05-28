bbf524a25861cdacad61c9e25015c06ecf3c90eb
old-node <olivier.ld@gmail.com>
2017-05-28 17:53:27 -0400

    initial commit

---
+++ tron/tron/tron.cpp
@@ -0,0 +1,35 @@
+/* En-tête du fichier */
+///================== */
+/* Fichier:			tron.cpp
+// Auteur:			Olivier Lemay Dostie & Francis Forest
+// Date création:	28/05/2017
+// Description:		Projet en SFML basé sur le jeu de TRON.*/
+
+/* Directives au pré-processeur */
+///============================ */
+#include <iomanip>				
+#include <locale>				
+#include <string>				
+#include <vector>				
+#include <cstdlib>				
+using namespace std;
+#include <SFML/Graphics.hpp>	
+#include <SFML/Audio.hpp>		
+#include "saisieSFML.h"			
+#include "bolide.h"				
+#include "trace.h"				
+#include "salle.h"				
+using namespace sf;
+
+
+/* Prototypes des fonctions */
+///======================== */
+
+
+
+
+/* Programme principal */
+///=================== */
+int main() {
+
+}
